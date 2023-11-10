b6fb0dbcff915e70609d47d9fe5b54f44bf133a0
IvoSa92 <ivo_sauter@web.de>
2023-11-10 18:29:23 +0100

    created a Button and an event listener witch activates the functions to create a grid and to add the class .sqaurehover to it.
    changed the CSS properties of the grid container inside the JS function to match the grid rows and columns.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const gridContainer = document.querySelector(".grid-container");
+// Function to create a Grid
 
 function grid(squares) {
   for (i = 0; i < squares; i++) {
@@ -8,12 +9,37 @@ function grid(squares) {
   }
 }
 
-grid(256);
+let allSquares = document.querySelectorAll(".square");
 
-const allSquares = document.querySelectorAll(".square");
-
-for (j = 0; j < allSquares.length; j++) {
+// Function to add a Class to every square
+/*for (j = 0; j < allSquares.length; j++) {
   allSquares[j].addEventListener("mouseenter", function () {
     this.classList.add("square-hover");
   });
-}
+}*/
+
+// Creating a Button and a Function witch creates as many squares as the user inputs
+
+const button = document.querySelector("button");
+let input;
+
+button.addEventListener("click", function () {
+  gridContainer.innerHTML = "";
+  let input = prompt("Enter Grid Size between 0 - 100");
+  let squares = input * input;
+  gridContainer.style.gridTemplateColumns = `repeat(${input},1fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${input},1fr)`;
+
+  if (input <= 100) {
+    grid(squares);
+  } else {
+    alert("Please enter a Number between 0-100 :)");
+  }
+  input = 0;
+  const allSquares = document.querySelectorAll(".square");
+  for (j = 0; j < allSquares.length; j++) {
+    allSquares[j].addEventListener("mouseenter", function () {
+      this.classList.add("square-hover");
+    });
+  }
+});
